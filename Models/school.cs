7925178581f0de135933df873a57a2bf7d96e787
Aishwarya Karanth <35144851+AishwaryaKaranth@users.noreply.github.com>
2021-06-28 20:53:35 +0530

    Add files via upload

---
+++ Models/school.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class School
+    {
+        public string schoolName;
+        public School(string schoolName)
+        {
+            this.schoolName = schoolName;
+        }
+    }
+}
---
+++ Models/student.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System.Linq;
+
+namespace ConsoleApp{
+    class Student
+    {
+        public string name;
+        public int rollNumber;
+        public List<int> marks = new List<int>();
+
+        public Student(string name, int rollNumber)
+        {
+            this.name = name;
+            this.rollNumber = rollNumber;
+        }
+
+        public Student(List<int> marks)
+        {
+            this.marks = marks;
+        }
+
+
+    }
+
+}
